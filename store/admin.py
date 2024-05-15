38bef039cd3acad70946081c502cb3af146cc77a
musthafaqwe123 <muthurdz123@gmail.com>
2024-05-15 18:44:35 +0530

    trendbazar

---
+++ store/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from store.models import Category,Brand,Size,Product,Order
+admin.site.register(Category)
+admin.site.register(Brand)
+admin.site.register(Size)
+admin.site.register(Product)
+admin.site.register(Order)
+# Register your models here.
+
